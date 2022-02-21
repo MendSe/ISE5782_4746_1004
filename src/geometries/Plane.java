b13e8b30cd17b49d0305835e0ddc6b9c56fd313e
lior serraf <lior.serraf@gmail.com>
2022-02-22 01:08:21 +0200

    je suis comitted

--- src/geometries/Plane.java
+++ src/geometries/Plane.java
@@ -11,6 +11,13 @@ public class Plane implements Geometry{
     }
 
     public Plane(Point d1,Point d2,Point d3) {
-        Vector norm=
+        Vector vec1 = d1.subtract(d2);
+        Vector vec2 = d1.subtract(d3);
+        Vector norm = vec1.crossProduct(vec2);
+        this.normal = null;
     }
+    public  Plane(Vector vec){
+        this.normal=null;
+    }
+
 }
--- src/geometries/Triangle.java
+++ src/geometries/Triangle.java
@@ -3,5 +3,12 @@ package geometries;
 import primitives.Point;
 import primitives.Vector;
 
+import java.util.List;
+
 public class Triangle extends Polygon{
+    Triangle (Point d1,Point d2,Point d3){
+      super(d1,d2,d3);
+        //  super(new List<Point>(){{add(d1);add(d2);add(d3);}});
+
+    }
 }
