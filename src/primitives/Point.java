a6687674221e0f602eee2ca3f18a547d4651a71f
Mendel Setti <mendel.setti@gmail.com>
2022-02-22 22:44:04 +0200

    Je roucoule

--- src/primitives/Point.java
+++ src/primitives/Point.java
@@ -18,22 +18,34 @@ public class Point {
                  xyz ;
     }
 
+    /**
+     * Constructor of the class which initializes the xyz variable of the class
+     * */
     public Point(double x1, double x2, double x3){
         xyz = new Double3(x1,x2,x3);
     }
 
+    /**
+     * This function returns a vector that's result of the subtraction of the point2 and xyz
+     * */
     public Vector subtract(Point p1)
     {
         Double3 help = xyz.subtract(p1.xyz);
         return new Vector(help.d1, help.d2, help.d3);
     }
 
+    /**
+     * This function add a vector to the point to return a point
+     * */
     public Point add(Vector vec)
     {
         Double3 help = xyz.add(vec.xyz);
         return new Point(help.d1, help.d2, help.d3);
     }
 
+    /**
+     * This function returns the squared distance between 2 points
+     * */
     public double DistanceSquared(Point point2)
     {
        double x= (xyz.d1-point2.xyz.d1)*(xyz.d1-point2.xyz.d1);
@@ -42,6 +54,9 @@ public class Point {
        return x+y+z;
     }
 
+    /**
+     * This function returns the distance between 2 points by computing the square root of the squared distance
+     * */
     public double Distance(Point point2)
     {
         return Math.sqrt(DistanceSquared(point2));
--- src/primitives/Vector.java
+++ src/primitives/Vector.java
@@ -5,6 +5,11 @@ import static primitives.Util.isZero;
 
 public class Vector extends Point{
 
+    /**
+     *This constructor initializes the values of the xyz variable by calling the constructor of the point class
+     * It also checks if the user entered the 0 vector and in this case it will throw an exception
+     *
+     */
     public Vector(double x1,double x2,double x3)
     {
         super(x1,x2,x3);
@@ -13,30 +18,45 @@ public class Vector extends Point{
 
     }
 
+    /**
+    * This function add a vector to our vector and return this new vector to the main
+    * */
     public Vector add(Vector vector2){
         Double3 help = xyz.add(vector2.xyz);
         return new Vector(help.d1, help.d2, help.d3);
     }
 
+
+    /**
+    * This function subtracts the vector2 to our vector and returns this new vector
+    * */
     public Vector subtract(Vector vector2)
     {
         Double3 help = xyz.subtract(vector2.xyz);
         return new Vector(help.d1, help.d2, help.d3);
     }
 
+    /**
+    * This function calculates the product of the scalar and our vector and then returns it
+    * */
     public Vector Scale(double scalar)
     {
-        //
         Double3 help= xyz.scale(scalar);
         return new Vector(help.d1, help.d2, help.d3);
     }
 
+    /**
+     * This function returns the dot product between vector2 and our vector
+     * */
     public double dotProduct(Vector vector2)
     {
         Double3 help = xyz.product(vector2.xyz);
         return help.d1+ help.d2+ help.d3;
     }
 
+    /**
+     * This functions calculates and returns the cross product between vector2 and our vector
+     * */
     public Vector crossProduct(Vector vector2)
     {
         double cx= xyz.d2*vector2.xyz.d3- xyz.d3*vector2.xyz.d2;
@@ -45,16 +65,25 @@ public class Vector extends Point{
         return new Vector(cx,cy,cz);
     }
 
+    /**
+     * This function calculates the square length of the vector and returns it
+     * */
     public double lengthSquared()
     {
         return (xyz.d1* xyz.d1)+ (xyz.d2* xyz.d2)+ (xyz.d3* xyz.d3);
     }
 
+    /**
+     * This function returns the length of the vector by calculating the square root of the squared length
+     * */
     public double length()
     {
         return Math.sqrt(this.lengthSquared());
     }
 
+    /**
+     * This functions returns the normalized vector of the vector
+     * */
     public Vector normalize()
     {
         double helpx,helpy,helpz;
@@ -65,10 +94,15 @@ public class Vector extends Point{
         helpz = xyz.d3/length;
         return new Vector(helpx,helpy,helpz);
     }
+
+    /**
+     * This function keeps the DRY concept by using the equal of the father class
+     * */
     @Override
     public boolean equals(Object o){
         return super.equals(o);
     }
+
     @Override
     public String toString() {
         return "Vector:"+ super.toString();
