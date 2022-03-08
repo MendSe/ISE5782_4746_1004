8d4060e705ecc50c3b6caa9ddcc891ea6ee533ba
Mendel Setti <mendel.setti@gmail.com>
2022-03-08 19:24:26 +0200

    commInter

--- unittests/primitives/VectorTest.java
+++ unittests/primitives/VectorTest.java
@@ -7,29 +7,34 @@ import static primitives.Util.isZero;
 
 class VectorTest {
 
+    Vector v1 = new Vector(1,2,3);
+    Vector v2 = new Vector(0, 3, -2);
     @Test
     void add() {
+        assertEquals(v1, new Vector(1,1,1).add(new Vector(0,1,2)),"Wrong vector Addition");
+
+        assertThrows(IllegalArgumentException.class,() ->new Vector(1,2,3).add(new Vector(-1,-2,-3)),"Vector 0 doesn't exist here");
     }
 
     @Test
     void subtract() {
+        assertEquals(new Vector(1,-1,5),v1.subtract(v2),"Wrong vector subtraction");
     }
 
     @Test
     void scale() {
+        assertEquals(new Vector(2,4,6),v1.Scale(2),"Wrong scaling");
+        assertThrows(IllegalArgumentException.class,()->v1.Scale(0),"We cannot scale by 0");
     }
 
     @Test
     void dotProduct() {
-        Vector v1 = new Vector(1,2,3);
+        assertEquals(0d,v1.dotProduct(v2),0.01,"Wrong dot product");
     }
 
     @Test
     void crossProduct() {
-        Vector v1 = new Vector(1, 2, 3);
-
         // ============ Equivalence Partitions Tests ==============
-        Vector v2 = new Vector(0, 3, -2);
         Vector vr = v1.crossProduct(v2);
 
 
@@ -52,13 +57,18 @@ class VectorTest {
 
     @Test
     void lengthSquared() {
+        assertEquals(14d,v1.lengthSquared(),"Wrong length squared");
     }
 
     @Test
     void length() {
+        assertEquals(5d,new Vector(0,3,4).length(),0.0001,"Wrong length computation");
     }
 
     @Test
     void normalize() {
+        Vector normal= v1.normalize();
+        //assertTrue(v1 == normal,"normalize is different from the actual vector");
+        assertEquals(1d,v1.normalize().length(),0.00001,"Wrong normalize");
     }
 }
