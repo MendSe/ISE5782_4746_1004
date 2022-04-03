c8d9bf02fc906bdcb1dd6f7b0bc7dbeee68c8ddd
lior serraf <lior.serraf@gmail.com>
2022-04-03 23:09:58 +0300

    comm tests

--- unittests/geometries/SphereTest.java
+++ unittests/geometries/SphereTest.java
@@ -27,7 +27,7 @@ class SphereTest {
     void findIntsersections() {
         Sphere sp = new Sphere(new Point(2, 0, 0), 2d);
         // ============ Equivalence Partitions Tests ==============
-        //EP01: Ray intersect sphere in 2 points
+        //TC01: Ray intersect sphere in 2 points
         Point p1 = new Point(0.267949192431122, -1, 0);
         Point p2 = new Point(3.732050807568877, -1, 0);
         List<Point> result = sp.findIntsersections(new Ray(new Point(0, -1, 0), new Vector(4, 0, 0)));
@@ -36,67 +36,67 @@ class SphereTest {
             result = List.of(result.get(1), result.get(0));
         assertEquals(List.of(p1, p2), result, "Bad intersections when ray intersects the Sphere 2 times");
 
-        //EP02: Ray in Sphere
+        //TC02: Ray in Sphere
         p1 = new Point(3.732050807568877, -1, 0);
         result = sp.findIntsersections(new Ray(new Point(1, -1, 0), new Vector(3, 0, 0)));
         assertEquals(1, result.size(), "Wrong number of points");
         assertEquals(List.of(p1), result, "Bad intersections when ray is in the sphere and intersects the Sphere");
 
-        //EP03: Ray, only tail intersect
+        //TC03: Ray, only tail intersect
         assertNull(sp.findIntsersections(new Ray(new Point(4, -1, 0), new Vector(3, 0, 0))), "Bad intersections when ray doesn't intersect the sphere but his tail would intersect it ");
 
-        //EP04: Ray and tail no intersections
+        //TC04: Ray and tail no intersections
         assertNull(sp.findIntsersections(new Ray(new Point(0, -3, 0), new Vector(3, 0, 0))), "Bad intersections when nor ray nor his tail intersects the sphere ");
 
 
         // =============== Boundary Values Tests ==================
         // **** Group: Ray's line crosses the sphere (but not the center)
-        //BVA01: P0 on the Sphere and Ray out of it
+        //TC05: P0 on the Sphere and Ray out of it
         assertNull(sp.findIntsersections(new Ray(new Point(0, 0, 0), new Vector(-1, -1, 0))), "Bad intersections when P0 on the Sphere and ray out of it ");
 
-        //BVA02: P0 on the Sphere and Ray intersect the sphere in
+        //TC06: P0 on the Sphere and Ray intersect the sphere in
         result = sp.findIntsersections(new Ray(new Point(0.267949192431122, -1, 0), new Vector(4, 0, 0)));
         p1 = new Point(3.732050807568877, -1, 0);
         assertEquals(1, result.size(), "Wrong number of points");
         assertEquals(result, List.of(p1), "Bad intersections when P0 is on the sphere and Ray intersects the Sphere ");
 
         // **** Group: Ray's line is tangent to the sphere
-        //BVA03: Ray tangent to sphere
+        //TC07: Ray tangent to sphere
 
         assertNull(sp.findIntsersections(new Ray(new Point(0, -2, 0), new Vector(4, 0, 0))), "Bad intersection when the Ray is tangent to the sphere");
 
-        //BVA04: P0 on Sphere Ray tangent to sphere
+        //TC08: P0 on Sphere Ray tangent to sphere
         assertNull(sp.findIntsersections(new Ray(new Point(2, -2, 0), new Vector(4, 0, 0))), "Bad intersection when P0 on the sphere and the Ray is tangent to it");
 
-        //BVA05: tail tangent to the sphere
+        //TC09: tail tangent to the sphere
         assertNull(sp.findIntsersections(new Ray(new Point(3, -2, 0), new Vector(4, 0, 0))), "Bad intersection when tail of the Ray is tangent to the sphere");
 
         // **** Group: Ray's line goes through the center
-        //BVA06: P0 on the sphere and the tail intersect with the center
+        //TC10: P0 on the sphere and the tail intersect with the center
         assertNull(sp.findIntsersections(new Ray(new Point(0, 0, 0), new Vector(-4, 0, 0))), "Bad intersection when P0 is on the sphere and the tail intersect the center");
 
-        //BVA07: P0 and the center are the same point
+        //TC11: P0 and the center are the same point
         result = sp.findIntsersections(new Ray(new Point(2, 0, 0), new Vector(4, 0, 0)));
         p1 = new Point(4, 0, 0);
         assertEquals(1, result.size(), "Wrong number of points");
         assertEquals(result, List.of(p1), "Bad intersection when P0 and the center are the same point");
 
-        //BVA08: Ray starts after the sphere and his tail intersect the center
+        //TC12: Ray starts after the sphere and his tail intersect the center
         assertNull(sp.findIntsersections(new Ray(new Point(5, 0, 0), new Vector(1, 0, 0))), "Bad intersection when the ray starts after the sphere and his tail intersect the center");
 
-        //BVA09: P0 on the sphere and ray goes through the center
+        //TC13: P0 on the sphere and ray goes through the center
         result = sp.findIntsersections(new Ray(new Point(0, 0, 0), new Vector(5, 0, 0)));
         p1 = new Point(4, 0, 0);
         assertEquals(1, result.size(), "Wrong number of points");
         assertEquals(result, List.of(p1), "Bad intersection when P0 is on the sphere and the ray goes through the center");
 
-        //BVA10: Ray start in the sphere and his tail intersect the center
+        //TC14: Ray start in the sphere and his tail intersect the center
         result = sp.findIntsersections(new Ray(new Point(3, 0, 0), new Vector(4, 0, 0)));
         p1 = new Point(4, 0, 0);
         assertEquals(1, result.size(), "Wrong number of points");
         assertEquals(result, List.of(p1), "Bad intersection when P0 is on the sphere and the ray goes through the center");
 
-        //BVA11: Ray start before the sphere and intersect it passing through the center
+        //TC15: Ray start before the sphere and intersect it passing through the center
         p1 = new Point(0, 0, 0);
         p2 = new Point(4, 0, 0);
         result = sp.findIntsersections(new Ray(new Point(-1, 0, 0), new Vector(6, 0, 0)));
@@ -106,7 +106,7 @@ class SphereTest {
         assertEquals(List.of(p1, p2), result, "Bad intersections when ray intersects the Sphere 2 times by going through his center");
 
         // **** Group: Special cases
-        //BVA12: Ray outside the sphere and perpendicular to it center
+        //TC16: Ray outside the sphere and perpendicular to it center
         assertNull(sp.findIntsersections(new Ray(new Point(2, -5, 0), new Vector(1, 0, 0))), "Bad intersection when the ray starts outside the sphere and perpendicular to his center");
     }
 }
--- unittests/primitives/VectorTest.java
+++ unittests/primitives/VectorTest.java
@@ -13,24 +13,35 @@ class VectorTest {
     Vector v2 = new Vector(0, 3, -2);
     @Test
     void add() {
+        // ============ Equivalence Partitions Tests ==============
+        //TC01: Simple test
         assertEquals(v1, new Vector(1,1,1).add(new Vector(0,1,2)),"Wrong vector Addition");
-
+        // =============== Boundary Values Tests ==================
+        //TC02: Vector 0
         assertThrows(IllegalArgumentException.class,() ->new Vector(1,2,3).add(new Vector(-1,-2,-3)),"Vector 0 doesn't exist here");
     }
 
     @Test
     void subtract() {
+        // ============ Equivalence Partitions Tests ==============
+        //TC01: Simple Test
         assertEquals(new Vector(1,-1,5),v1.subtract(v2),"Wrong vector subtraction");
     }
 
     @Test
     void scale() {
+        // ============ Equivalence Partitions Tests ==============
+        //TC01: Simple test
         assertEquals(new Vector(2,4,6),v1.scale(2),"Wrong scaling");
+        // =============== Boundary Values Tests ==================
+        //TC02: Vector 0
         assertThrows(IllegalArgumentException.class,()->v1.scale(0),"We cannot scale by 0");
     }
 
     @Test
     void dotProduct() {
+        // ============ Equivalence Partitions Tests ==============
+        //TC01: Simple Test
         assertEquals(0d,v1.dotProduct(v2),0.01,"Wrong dot product");
     }
 
@@ -38,8 +49,6 @@ class VectorTest {
     void crossProduct() {
         // ============ Equivalence Partitions Tests ==============
         Vector vr = v1.crossProduct(v2);
-
-
         // TC01: Test that length of cross-product is proper (orthogonal vectors taken
         // for simplicity)
         assertEquals(v1.length() * v2.length(), vr.length(), 0.00001,"crossProduct() wrong result length");
@@ -50,7 +59,7 @@ class VectorTest {
         assertTrue(isZero(vr.dotProduct(v2)),"crossProduct() result is not orthogonal to 2nd operand");
 
         // =============== Boundary Values Tests ==================
-        // TC11: test zero vector from cross-productof co-lined vectors
+        // TC03: test zero vector from cross-productof co-lined vectors
         Vector v3 = new Vector(-2, -4, -6);
         assertThrows(IllegalArgumentException.class, () -> v1.crossProduct(v3),
                 "crossProduct() for parallel vectors does not throw an exception");
@@ -59,18 +68,23 @@ class VectorTest {
 
     @Test
     void lengthSquared() {
+        // ============ Equivalence Partitions Tests ==============
+        //TC01: Simple test
         assertEquals(14d,v1.lengthSquared(),"Wrong length squared");
     }
 
     @Test
     void length() {
+        // ============ Equivalence Partitions Tests ==============
+        //TC01: Simple Test
         assertEquals(5d,new Vector(0,3,4).length(),0.0001,"Wrong length computation");
     }
 
     @Test
     void normalize() {
         Vector normal= v1.normalize();
-        //assertTrue(v1 == normal,"normalize is different from the actual vector");
+        // ============ Equivalence Partitions Tests ==============
+        //TC01: Simple Test
         assertEquals(1d,v1.normalize().length(),0.00001,"Wrong normalize");
     }
 }
