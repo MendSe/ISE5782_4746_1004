4c5e07843cea66057740c72b3abddfb7b05808b4
lior serraf <lior.serraf@gmail.com>
2022-04-25 23:09:30 +0300

    scene builder

--- src/scene/Scene.java
+++ src/scene/Scene.java
@@ -1,8 +1,10 @@
 package scene;
 
 import geometries.Geometries;
+import geometries.Intersectable;
 import lighting.AmbientLight;
 import primitives.Color;
+import primitives.Double3;
 
 public class Scene {
 
@@ -11,13 +13,27 @@ public class Scene {
     public AmbientLight ambientLight=new AmbientLight();
     public Geometries geometries=new Geometries();
 
-    Scene(String string){
+    private Scene(String string){
         this.name=string;
         geometries=new Geometries();
     }
 
-    public class Builder{
-
-
-    }
-}
+        public static class Builder {
+            private Scene scene = null;
+            public Builder(String name) { scene = new Scene(name); }
+            public Builder reset(String name) { scene = new Scene(name); return this; }
+            public Builder setBackground(Color color) { scene.background = color; return this; }
+            public Builder setAmbient(Color color, Double3 ka) {
+                scene.ambientLight = new AmbientLight(color, ka); return this;
+            }
+            public Builder addGeometry(Intersectable geo) {
+                if (scene.geometries == null) scene.geometries = new Geometries();
+                scene.geometries.add(geo);
+                return this;
+            }
+            public Scene build() {
+                if (scene.name == null || scene.geometries == null) throw new IllegalArgumentException("No name or no objects in the scene");
+                return scene;
+            }
+        } // class Builder
+    } // class Scene
