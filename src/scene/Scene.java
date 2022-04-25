cfe96df4ece186fd17f82f11fde4529b00c12269
lior serraf <lior.serraf@gmail.com>
2022-04-25 23:42:27 +0300

    rendertest

--- src/scene/Scene.java
+++ src/scene/Scene.java
@@ -9,31 +9,47 @@ import primitives.Double3;
 public class Scene {
 
     public String name;
-    public Color background=Color.BLACK;
-    public AmbientLight ambientLight=new AmbientLight();
-    public Geometries geometries=new Geometries();
+    public Color background = Color.BLACK;
+    public AmbientLight ambientLight = new AmbientLight();
+    public Geometries geometries = new Geometries();
 
-    private Scene(String string){
-        this.name=string;
-        geometries=new Geometries();
+    Scene(String string) {
+        this.name = string;
+        geometries = new Geometries();
     }
 
-        public static class Builder {
-            private Scene scene = null;
-            public Builder(String name) { scene = new Scene(name); }
-            public Builder reset(String name) { scene = new Scene(name); return this; }
-            public Builder setBackground(Color color) { scene.background = color; return this; }
-            public Builder setAmbient(Color color, Double3 ka) {
-                scene.ambientLight = new AmbientLight(color, ka); return this;
-            }
-            public Builder addGeometry(Intersectable geo) {
-                if (scene.geometries == null) scene.geometries = new Geometries();
-                scene.geometries.add(geo);
-                return this;
-            }
-            public Scene build() {
-                if (scene.name == null || scene.geometries == null) throw new IllegalArgumentException("No name or no objects in the scene");
-                return scene;
-            }
-        } // class Builder
-    } // class Scene
+    public static class Builder {
+        private Scene scene = null;
+
+        public Builder(String name) {
+            scene = new Scene(name);
+        }
+
+        public static Builder reset(String name) {
+            scene = new Scene(name);
+            return this;
+        }
+
+        public Builder setBackground(Color color) {
+            scene.background = color;
+            return this;
+        }
+
+        public Builder setAmbient(Color color, Double3 ka) {
+            scene.ambientLight = new AmbientLight(color, ka);
+            return this;
+        }
+
+        public Builder addGeometry(Intersectable geo) {
+            if (scene.geometries == null) scene.geometries = new Geometries();
+            scene.geometries.add(geo);
+            return this;
+        }
+
+        public Scene build() {
+            if (scene.name == null || scene.geometries == null)
+                throw new IllegalArgumentException("No name or no objects in the scene");
+            return scene;
+        }
+    } // class Builder
+} // class Scene
