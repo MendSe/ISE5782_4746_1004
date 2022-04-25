f7ec694bfdbefe9f3d358a7ca2e4af5b0e0d8036
lior serraf <lior.serraf@gmail.com>
2022-04-25 23:19:56 +0300

    scene builder

--- unittests/primitives/RayTest.java
+++ unittests/primitives/RayTest.java
@@ -3,17 +3,22 @@ package primitives;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import org.junit.jupiter.api.Test;
+import primitives.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class RayTest {
 
-    Ray ray = new Ray(new Point(1,1,1),new Vector(-1,1,2));
+    @Test
+    void findClosestPoint() {
+        Ray ray = new Ray(new Point(1, 1, 1), new Vector(-1, 1, 2));
 
-    //============Equivalence Partition==============
+        //============Equivalence Partition==============
 
-    List<Point> p1 = new ArrayList<>();
-    p1.add()
+        List<Point> p1 = new ArrayList<Point>();
 
 
+
+    }
 }
