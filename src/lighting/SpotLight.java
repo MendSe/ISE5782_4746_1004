af3609f9d47862e0326851da1cba7df07ac82359
lior serraf <lior.serraf@gmail.com>
2022-05-09 21:23:38 +0300

    spotlight

--- src/lighting/SpotLight.java
+++ src/lighting/SpotLight.java
@@ -10,4 +10,15 @@ public class SpotLight extends PointLight{
         this.direction = direction.normalize();
     }
 
+    @Override
+ public Color getIntensity(Point p){
+        double dl = Util.alignZero(getL(p).dotProduct(this.direction));
+        if(dl <= 0) return Color.BLACK;
+        return super.getIntensity(p).scale(dl);
+         }
+
+    @Override
+    public Vector getL(Point p) {
+        return super.getL(p);
+    }
 }
