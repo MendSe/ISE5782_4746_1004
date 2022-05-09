e2c83ecf1dd0efd59aeb8540665f267cfd20bbba
Mendel Setti <mendel.setti@gmail.com>
2022-05-09 18:05:56 +0300

    lighting

--- src/lighting/DirectionalLight.java
+++ src/lighting/DirectionalLight.java
@@ -1,7 +1,22 @@
 package lighting;
-import primitives.Vector;
+import primitives.*;
 
-public class DirectionalLight {
+public class DirectionalLight extends Light implements LightSource {
     private Vector direction;
+
+    public DirectionalLight(Color intensity,Vector direction) {
+        super(intensity);
+        this.direction = direction.normalize();
+    }
+
+    @Override
+    public Color getIntensity(Point p) {
+        return this.getIntensity();
+    }
+
+    @Override
+    public Vector getL(Point p){
+        return direction;
+    }
 
 }
--- src/lighting/SpotLight.java
+++ src/lighting/SpotLight.java
@@ -1,7 +1,13 @@
 package lighting;
+import com.sun.jdi.connect.Transport;
 import primitives.*;
 
 public class SpotLight extends PointLight{
     private Vector direction;
+
+    public SpotLight(Color intensity,Point position, Vector direction){
+        super(intensity,position);
+        this.direction = direction.normalize();
+    }
 
 }
