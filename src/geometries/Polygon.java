f2baac16ed46ef24fd9996e9d2b4961cdb637e67
Mendel Setti <mendel.setti@gmail.com>
2022-05-17 17:55:00 +0300

    polygone bonus partial

--- src/geometries/Polygon.java
+++ src/geometries/Polygon.java
@@ -91,5 +91,33 @@ public class Polygon extends Geometry {
     }
 
     @Override
-    protected List<GeoPoint> findGeoIntersectionsHelper(Ray ray,double maxDistance) { return null; }
+    protected List<GeoPoint> findGeoIntersectionsHelper(Ray ray,double maxDistance) {
+        List<GeoPoint> intersections = plane.findGeoIntersections(ray);
+        if(intersections==null) return null;
+
+        Point p0 = ray.getP0();
+        Point p1 = vertices.get(1);
+        Point p2 = vertices.get(0);
+
+        Vector v0 = ray.getDir();
+        Vector v1 = p1.subtract(p0);
+        Vector v2 = p2.subtract(p0);
+
+        double sign = alignZero(v0.dotProduct(v1.crossProduct(v2)));
+        if(isZero(sign)) return null;
+
+        boolean positive = (sign>0);
+        for(int i=vertices.size()-1;i>0;i++){
+            v1=v2;
+            v2 = vertices.get(i).subtract(p0);
+
+            sign = alignZero(v0.dotProduct(v1.crossProduct(v2)));
+            if(isZero(sign)) return null;
+            if(positive != (sign>0)) return null;
+        }
+
+        for(GeoPoint gp : intersections) gp.geometry=this;
+
+        return intersections;
+    }
 }
